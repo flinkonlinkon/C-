f09bcef9fde199beca4411695104ea3844422457
Linkon <flinkon9@gmail.com>
2025-06-19 00:58:29 +0600

    update

---
+++ diamond.c
@@ -0,0 +1,40 @@
+
+#include <stdio.h>
+
+int main(){
+
+int n;
+scanf("%d",&n);
+int nst = 1;
+int nsp = n/2;
+int ml = n/2 + 1;
+
+
+for(int i = 1;i<=n;i++){
+
+    for(int k=1;k<=nsp;k++){
+        printf(" ");
+    }
+    for(int j=1;j<= nst ;j++){
+        printf("*");
+
+    }
+
+    if(i<ml){
+        nsp--;
+        nst+=2;
+    }
+    else{
+      nsp++;
+        nst-=2;
+    }
+
+    printf("\n");
+}
+
+
+
+
+return 0;
+}
+
