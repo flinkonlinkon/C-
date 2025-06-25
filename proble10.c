f6cff286b5e585fc42af613a2e5e4ce6f03ebb25
Linkon <flinkon9@gmail.com>
2025-06-25 12:23:19 +0600

    update

--- proble10.c
+++ proble10.c
@@ -1,1 +1,18 @@
+#include<stdio.h>
 
+int main() {
+    int code1, qty1, code2, qty2;
+    float price1, price2;
+
+
+    scanf("%d %d %f", &code1, &qty1, &price1);
+
+
+    scanf("%d %d %f", &code2, &qty2, &price2);
+
+    float total = (qty1 * price1) + (qty2 * price2);
+
+    printf("VALOR A PAGAR: R$ %.2f\n", total);
+
+    return 0;
+}
